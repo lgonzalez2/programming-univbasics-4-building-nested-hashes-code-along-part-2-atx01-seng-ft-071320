7cca7dc97d95b6deb98650b288f6e7b686bbb0bb
Lucio Gonzalez <lgonzalez_94@hotmail.com>
2020-06-30 23:07:21 +0000

    Done.

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -48,10 +48,7 @@ def add_characters
       }
    }
    
-   epic_tragedy[:additional_characters] = [
-     {name: "Prince Escalus"}, 
-     {name: "Apothecary"}
-     ]
+   epic_tragedy[:additional_characters] = [{name: "Prince Escalus"}, {name: "Apothecary"}]
 
    epic_tragedy
 end
